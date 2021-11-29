31a35260fc404ea841e0a861375ff7cf91455ee6
Ehsanghaffarii <ghafari.5000@gmail.com>
2021-11-29 21:49:32 +0330

    remove no need files

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -17,9 +17,8 @@
     <meta name="twitter:image" content=""> --}}
     <meta name="twitter:site" content="@ehsanghaffarii">
     <meta name="twitter:creator" content="@ehsanghaffarii">
-    <!-- <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}"> -->
     <link rel="stylesheet" href="{{ asset('css/neumorphism/neumorphism.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css')}}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/main.css')}}">
     @yield('styles')
 </head>
@@ -30,10 +29,6 @@
 @yield('content')
 
 @include('site.layouts.footer')
-
-<!-- <script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script> -->
-<!-- <script src="{{ asset('vendor/popper.js/dist/umd/popper.min.js') }}"></script> -->
-<!-- <script src="{{ asset('js/bootstrap.min.js') }}"></script> -->
 <script src="{{ asset('js/main.js')}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
-mix.copy('node_modules/font-awesome/css/font-awesome.min.css', 'public/css'); 
 mix.copy('node_modules/font-awesome/fonts/*', 'public/fonts');
-mix.copy('node_modules/bootstrap/dist/js/bootstrap.min.js', 'public/js');
-mix.copy('node_modules/bootstrap/dist/css/bootstrap.css', 'public/css')
 
